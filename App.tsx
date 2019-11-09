46d56a5ff72498c4afc6d11623b05f4d87420353
rizkyramadhan <rizky@andromedia.co.id>
2019-11-09 11:58:07 +0700

    fix

--- App.tsx
+++ App.tsx
@@ -1,19 +1,9 @@
 import CactivaForm from "@src/CactivaForm";
 import { observer } from "mobx-react-lite";
 import React from "react";
-import { createStackNavigator } from "react-navigation-stack";
 import { AppContainer } from "./src/libs/navigation";
-const StackNav = createStackNavigator(
-  {
-    // Home,
-    CactivaForm
-  },
-  {
-    headerMode: "none"
-  }
-);
-export const Container = AppContainer(StackNav);
 
+const Container = AppContainer();
 export default observer(() => {
   return (
     <Container
